934d39d7c7c9f88f73828ccdb68ae46de7dcc7c4
Subhashish Pradhan <sprkv5@gmail.com>
2014-05-30 20:10:01 -0400

    vki-gnu.h - added symbols from stat.h

--- include/vki/vki-gnu.h
+++ include/vki/vki-gnu.h
@@ -31,6 +31,7 @@
 #ifndef __VKI_GNU_H
 #define __VKI_GNU_H
 
+
 #include <stdint.h>
 
 #define vki_int8_t int8_t
@@ -42,6 +43,7 @@
 #define vki_int64_t int64_t
 #define vki_uint64_t uint64_t
 
+
 // #include <malloc.h>
 // tbd (to be done)
 
@@ -52,3 +54,66 @@
 #define vki_tm tm
 #define vki_itimerspec itimerspec
 #define vki_sigevent sigevent
+
+
+// Since there's no upper limit on the length of a path as is the
+// case with GNU, VKI_PATH_MAX is set arbitrarily.
+
+#define VKI_PATH_MAX 4096
+
+
+#include <i386-gnu/sys/stat.h>
+
+#define VKI_S_IFMT S_IFMT
+#define VKI_S_IFDIR S_IFDIR
+#define VKI_S_IFCHR S_IFCHR
+#define VKI_S_IFBLK S_IFBLK
+#define VKI_S_IFREG S_IFREG
+#define VKI_S_IFIFO S_IFIFO
+#define VKI_S_IFLNK S_IFLNK
+#define VKI_S_IFSOCK S_IFSOCK
+
+#define VKI_S_ISTYPE(md, ms)  __S_ISTYPE(md, ms)
+
+#define VKI_S_ISDIR(m) S_ISDIR(m)
+#define VKI_S_ISCHR(m) S_ISCHR(m)
+#define VKI_S_ISBLK(m) S_ISBLK(m)
+#define VKI_S_ISREG(m) S_ISREG(m)
+#define VKI_S_ISFIFO(m) S_ISFIFO(m)
+#define VKI_S_ISLNK(m) S_ISLNK(m)
+#define VKI_S_ISSOCK(m) S_ISSOCK(m)
+
+#define VKI_S_ISUID S_ISUID
+#define VKI_S_ISGID S_ISGID
+#define VKI_S_ISVTX S_ISVTX
+#define VKI_S_IRUSR S_IRUSR
+#define VKI_S_IWUSR S_IWUSR
+#define VKI_S_IXUSR S_IXUSR
+#define VKI_S_IRWXU S_IRWXU
+#define VKI_S_IRGRP S_IRGRP
+#define VKI_S_IWGRP S_IWGRP
+#define VKI_S_IXGRP S_IXGRP
+#define VKI_S_IRWXG S_IRWXG
+#define VKI_S_IROTH S_IROTH
+#define VKI_S_IWOTH S_IWOTH
+#define VKI_S_IXOTH S_IXOTH
+#define VKI_S_IRWXO S_IRWXO
+
+
+
+/*
+   #if defined __USE_BSD && !defined __S_IFLNK
+   # define S_ISLNK(mode)  0
+   #endif
+
+   #if defined __USE_MISC && defined __USE_BSD
+   # define S_IREAD    S_IRUSR
+   # define S_IWRITE   S_IWUSR
+   # define S_IEXEC    S_IXUSR
+   #endif
+
+   These are not required, I suppose.
+*/
+
+
+#endif
